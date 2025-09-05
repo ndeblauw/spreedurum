f39792f53d36fc1d05c93d23a8b85f1e6eae9c16
Nico Deblauwe <nico@bluepundit.eu>
2025-09-05 15:56:24 +0200

    FEATURE :sparkles: Add the author name to an article

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,9 +1,12 @@
 <x-site-layout>
 
-            <h1 class="text-4xl font-bold">{{$article->title}}</h1>
+    <h1 class="text-4xl font-bold">{{$article->title}}</h1>
 
-                <div>
-                    {{$article->content}}
-                </div>
+    <div class="mb-2 text-blue-800">by our reporter: {{$article->author->name}}</div>
+
+
+    <div>
+        {{$article->content}}
+    </div>
 
 </x-site-layout>
