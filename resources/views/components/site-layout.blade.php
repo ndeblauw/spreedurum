77b92dc31d67dbb583b106f469cd4d9b6f5106f7
Nico Deblauwe <nico@bluepundit.eu>
2025-09-05 11:48:33 +0200

    FEATURE :sparkles: Creating a template for the SiteLayout

---
+++ resources/views/components/site-layout.blade.php
@@ -0,0 +1,33 @@
+<html>
+<header>
+    <title>Articles overview</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</header>
+<body class="">
+<header class="max-w-6xl mx-auto bg-blue-500 text-xl text-white mb-4 flex items-center h-12">
+    <div class="mr-10">
+        SpreeDURUM!
+    </div>
+    <div>
+        <a href="/articles">All articles</a>
+        |
+        All authors
+    </div>
+</header>
+
+<main class="max-w-6xl mx-auto px-4 py-4">
+
+    {{$slot}}
+
+</main>
+
+
+<footer class="bg-blue-900 text-white mt-12 min-h-20">
+
+    <div class="max-w-6xl mx-auto px-20 py-4">
+        Footer content
+    </div>
+</footer>
+</body>
+
+</html>
