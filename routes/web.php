456c131882d15765820ece4b332f4bd8efed0dc2
Nico Deblauwe <nico@bluepundit.eu>
2025-09-12 16:05:43 +0200

    CLEANUP :broom: Simplifying the routes file

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
-use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
 Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index'] );
 Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show']);
 
 Route::get('authors', [AuthorController::class, 'index']);
 Route::get('authors/{id}', [AuthorController::class, 'show']);
-
-
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-});
 
 require __DIR__.'/auth.php';
