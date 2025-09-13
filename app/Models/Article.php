d960dd39a82a8b3c62b88f04f65aa9db051cb177
Nico Deblauwe <nico@bluepundit.eu>
2025-09-13 15:01:48 +0200

    BUGFIX :bug: Make sure that properties are not guarded

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -10,6 +10,8 @@ class Article extends Model
     /** @use HasFactory<\Database\Factories\ArticleFactory> */
     use HasFactory;
 
+    protected $guarded = [];
+
     public function author()
     {
         return $this->belongsTo(User::class, 'author_id');
