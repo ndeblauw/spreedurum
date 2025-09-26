9ae9b82713714ca3685c94fa73e32f02e088d7ef
Nico Deblauwe <nico@bluepundit.eu>
2025-09-26 12:09:09 +0200

    FEATURE :sparkles: Don't loose old form content

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,7 +7,7 @@
 
         <div>
             <label for="title"> Title</label><br/>
-            <input type="text" name="title" class="bg-gray-200 p-2">
+            <input type="text" name="title" class="bg-gray-200 p-2" value="{{old('title')}}"><br/>
             @error('title')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
             @enderror
@@ -15,7 +15,7 @@
 
         <div>
             <label for="content">Content</label><br/>
-            <textarea name="content" class="bg-gray-200 p-2"></textarea>
+            <textarea name="content" class="bg-gray-200 p-2 w-1/2">{{old('content')}}</textarea><br/>
             @error('content')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
             @enderror
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -7,7 +7,7 @@
 
         <div>
             <label for="title">Title</label><br/>
-            <input type="text" name="title" class="bg-gray-200 p-2" value="{{$article->title}}">
+            <input type="text" name="title" class="bg-gray-200 p-2" value="{{old('title',$article->title)}}"><br/>
             @error('title')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
             @enderror
@@ -15,7 +15,7 @@
 
         <div>
             <label for="content">Content</label><br/>
-            <textarea name="content" class="bg-gray-200 p-2 w-1/2">{{$article->content}}</textarea>
+            <textarea name="content" class="bg-gray-200 p-2 w-1/2">{{old('content', $article->content)}}</textarea><br/>
             @error('content')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
             @enderror
