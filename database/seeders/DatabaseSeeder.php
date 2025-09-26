337dd90370c4a9c0277fd51c2c346dfc8075dfd1
Nico Deblauwe <nico@bluepundit.eu>
2025-09-26 15:47:49 +0200

    FEATURE :sparkles: Seed admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+
         \App\Models\User::factory(10)->create();
 
         \App\Models\Article::factory(10)->create();
