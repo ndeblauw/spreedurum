23b9ea8b82b70b08f784e273693de3736f3165c5
Nico Deblauwe <nico@bluepundit.eu>
2025-09-26 20:35:45 +0200

    BUGFIX :bug: Cleanup tests (and controllers) to deal with removed dashboard route

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -26,12 +26,17 @@ test('email can be verified', function () {
         ['id' => $user->id, 'hash' => sha1($user->email)]
     );
 
+    dump($verificationUrl);
+    dump($user);
+
     $response = $this->actingAs($user)->get($verificationUrl);
+
+    dump($response->status());
 
     Event::assertDispatched(Verified::class);
 
     expect($user->fresh()->hasVerifiedEmail())->toBeTrue();
-    $response->assertRedirect(route('dashboard', absolute: false).'?verified=1');
+    $response->assertRedirect(route('home', absolute: false).'?verified=1');
 });
 
 test('email is not verified with invalid hash', function () {
